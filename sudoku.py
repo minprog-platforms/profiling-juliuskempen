09790f6fcf45459252abf6d9e0cd7bc87fef8df4
juliuskempen <juliuskempen13@gmail.com>
2021-11-26 13:53:51 +0100

    profiling

--- sudoku.py
+++ sudoku.py
@@ -1,73 +1,73 @@
 from __future__ import annotations
-from typing import Iterable, Sequence
+from typing import Iterable, List
 
 
 class Sudoku:
     """A mutable sudoku puzzle."""
 
     def __init__(self, puzzle: Iterable[Iterable]):
-        self._grid: list[str] = []
+        self._grid: List[List[int]] = []
+        self._columns: List[List[int]] = []
+        self._blocks: List[Iterable[int]] = []
 
+        # Saving the grid by row.
         for puzzle_row in puzzle:
-            row = ""
-
+            row = []
             for element in puzzle_row:
-                row += str(element)
-
+                row.append(int(element))
             self._grid.append(row)
+
+        # Saving the grid by column.
+        for x in range(9):
+            column = []
+            for y in range(9):
+                column.append(self._grid[y][x])
+            self._columns.append(column)
+
+        # Saving the grid by block.
+        for i in range(9):
+            values = []
+            x_start = (i % 3) * 3
+            y_start = (i // 3) * 3
+            for x in range(x_start, x_start + 3):
+                for y in range(y_start, y_start + 3):
+                    values.append(self.value_at(x, y))
+            self._blocks.append(values)
 
     def place(self, value: int, x: int, y: int) -> None:
         """Place value at x,y."""
-        row = self._grid[y]
-        new_row = ""
+        self._grid[y][x] = value
+        self._columns[x][y] = value
 
-        for i in range(9):
-            if i == x:
-                new_row += str(value)
-            else:
-                new_row += row[i]
+        # Get the index of the block based from x,y.
+        block_index = (y // 3) * 3 + x // 3
 
-        self._grid[y] = new_row
+        # Replace the old block with the new.
+        self._blocks[block_index] = self.block_values(block_index)
 
     def unplace(self, x: int, y: int) -> None:
         """Remove (unplace) a number at x,y."""
-        row = self._grid[y]
-        new_row = row[:x] + "0" + row[x + 1:]
-        self._grid[y] = new_row
+        self.place(0, x, y)
 
     def value_at(self, x: int, y: int) -> int:
         """Returns the value at x,y."""
-        value = -1
-
-        for i in range(9):
-            for j in range(9):
-                if i == x and j == y:
-                    row = self._grid[y]
-                    value = int(row[x])
-
-        return value
+        return self._grid[y][x]
 
     def options_at(self, x: int, y: int) -> Iterable[int]:
         """Returns all possible values (options) at x,y."""
         options = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-        # Remove all values from the row
-        for value in self.row_values(y):
-            if value in options:
-                options.remove(value)
+        # Remove all values that are cointained in the row.
+        options = list(set(options) - set(self._grid[y]))
 
-        # Remove all values from the column
-        for value in self.column_values(x):
-            if value in options:
-                options.remove(value)
+        # Remove all values that are cointained in the column.
+        options = list(set(options) - set(self._columns[x]))
 
-        # Get the index of the block based from x,y
+        # Get the index of the block based on x,y.
         block_index = (y // 3) * 3 + x // 3
 
-        # Remove all values from the block
-        for value in self.block_values(block_index):
-            if value in options:
-                options.remove(value)
+        # Remove all values that are cointained in the block.
+        options = list(set(options) - set(self._blocks[block_index]))
 
         return options
 
@@ -80,28 +80,18 @@ class Sudoku:
 
         for y in range(9):
             for x in range(9):
-                if self.value_at(x, y) == 0 and next_x == -1 and next_y == -1:
-                    next_x, next_y = x, y
+                if self.value_at(x, y) == 0:
+                    return x, y
 
         return next_x, next_y
 
     def row_values(self, i: int) -> Iterable[int]:
         """Returns all values at i-th row."""
-        values = []
-
-        for j in range(9):
-            values.append(self.value_at(j, i))
-
-        return values
+        return self._grid[i]
 
     def column_values(self, i: int) -> Iterable[int]:
         """Returns all values at i-th column."""
-        values = []
-
-        for j in range(9):
-            values.append(self.value_at(i, j))
-
-        return values
+        return self._columns[i]
 
     def block_values(self, i: int) -> Iterable[int]:
         """
@@ -129,26 +119,24 @@ class Sudoku:
         """
         values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-        result = True
-
         for i in range(9):
-            for value in values:
-                if value not in self.column_values(i):
-                    result = False
+            if list(set(values) - set(self._grid[i])):
+                return False
+            if list(set(values) - set(self._columns[i])):
+                return False
+            if list(set(values) - set(self._blocks[i])):
+                return False
 
-                if value not in self.row_values(i):
-                    result = False
-
-                if value not in self.block_values(i):
-                    result = False
-
-        return result
+        return True
 
     def __str__(self) -> str:
+        """Convert list of intergers for every row to a string"""
         representation = ""
 
-        for row in self._grid:
-            representation += row + "\n"
+        for row in range(9):
+            for i in range(9):
+                representation += str(self._grid[row][i])
+            representation = representation + "\n"
 
         return representation.strip()
 
